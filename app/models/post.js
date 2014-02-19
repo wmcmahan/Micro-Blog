d32e1f859d8bf531363ec3a00ec5460ef05fe27b
wmcmahan <will_mcmahan@volusion.com>
2014-02-19 09:25:25 -0600

    edit typo

--- app/models/post.js
+++ app/models/post.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema
 
 
-// Shema
+// Shcema
 var ArticleSchema =  new Schema({
     title: { type: String, required: true },
     content: String,
